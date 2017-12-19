60b4282810ce1e68d219ddafa88f5df2c10e5007
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2017-12-19 15:29:53 -0200

    Use MustConnect instead of Connect

--- watchdog/database.go
+++ watchdog/database.go
@@ -1,15 +1,11 @@
 package watchdog
 
 import (
-	"github.com/apex/log"
 	"github.com/jmoiron/sqlx"
 )
 
 func connect(dataSourceName string) *sqlx.DB {
-	db, err := sqlx.Connect("postgres", dataSourceName)
-	if err != nil {
-		log.WithError(err).Fatal("Failed to open connection to database")
-	}
+	db := sqlx.MustConnect("postgres", dataSourceName)
 	db.SetMaxOpenConns(2)
 	return db
 }
